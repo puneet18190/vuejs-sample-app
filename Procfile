b6ac8c98c87787cdc3fd3acad5b664e8d635e621
Puneet <puneetgupta18190@gmail.com>
2020-02-28 23:34:52 +0530

    added procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec rails server -p 3000
+frontend: ./bin/webpack-dev-server
--- app/javascript/home/index.vue
+++ app/javascript/home/index.vue
@@ -9,7 +9,7 @@
 export default {
   data() {
     return {
-      message: "Hello Vue!. I am on Home Index Page",
+      message: "Hello Vue!. I am on Home Index Page!",
     }
   },
 }
